2cc2230284ab85ec0e92795e12f443e8a4cf577b
Farace <alessandro.farace@me.com>
2018-06-18 22:37:56 +0100

    Solves Caeser Cipher

--- Cryptography/Caeser.cs
+++ Cryptography/Caeser.cs
@@ -1,6 +1,7 @@
 ﻿namespace Cryptography
 {
     public enum Mode { Alphabet, CharacterUTF8, }
+
     public static class CaeserCipher
     {
         public static string Encrypt(string PlainText, int Key, Mode mode = Mode.Alphabet)
@@ -20,17 +21,30 @@
                 {
                     CipherText += (char)(((int)(character) + Key) % 256);
                 }
-
             }
             return CipherText;
         }
+
         public static string Decrypt(string CipherText, int Key, Mode mode = Mode.Alphabet)
         {
             return Encrypt(CipherText, -Key, mode);
         }
+
         public static string Solve(string CipherText, Mode mode = Mode.Alphabet)
         {
-            return null;
+            int bestKey = 0;
+            float score = 0;
+            for (int key = 0; key <= 25; key++)
+            {
+                string temp = Decrypt(CipherText, key);
+                float tempscore = LetterFrequency.GenerateFrequency(temp).Compare(LetterFrequency.EnglishLetterFrequency);
+                if (tempscore > score)
+                {
+                    score = tempscore;
+                    bestKey = key;
+                }
+            }
+            return Decrypt(CipherText, bestKey);
         }
     }
 }
